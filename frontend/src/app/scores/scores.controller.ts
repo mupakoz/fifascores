b4f2360cf547c10bd5201513eefbcb53a1474ec2
Pawel Kozikowski <mupakoz@gmail.com>
2015-11-07 11:58:19 +0100

    Fontawesome in dist fixed, confirm dialog to delete score added

--- frontend/src/app/scores/scores.controller.ts
+++ frontend/src/app/scores/scores.controller.ts
@@ -18,12 +18,14 @@ export class ScoresController {
     public static $inject = [
         '$scope',
         'ScoresService',
-        '$location'
+        '$location',
+        '$mdDialog'
     ];
 
     constructor(private $scope:IScoresScope,
                 private scoresService:Services.ScoresService,
-                private $location:ng.ILocationService) {
+                private $location:ng.ILocationService,
+                private $mdDialog) {
         $scope.vm = this;
         var that = this;
 
@@ -56,6 +58,22 @@ export class ScoresController {
             that.$scope.data = data;
         }).error(function () {
             console.log('Error when receiving data!');
+        });
+    }
+
+    public confirmDelete(ev,id:string):void {
+        // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = this.$mdDialog.confirm()
+            .title('Potwierdź usunięcie wyniku')
+            .content('Czy jesteś pewny że chcesz usunąć ten wynik?')
+            .ariaLabel('Lucky day')
+            .targetEvent(ev)
+            .ok('Tak!')
+            .cancel('Nie...');
+
+        var that: ScoresController = this;
+        this.$mdDialog.show(confirm).then(function () {
+            that.delete(id);
         });
     }
 
